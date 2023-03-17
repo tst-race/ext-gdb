f9fb70e22ef3e7696b4478e4fddaf7d488e4160b
Kyle Treubig <kyle.treubig@twosixlabs.com>
2023-03-17 10:55:53 -0400

    Only perform native builds

--- build.py
+++ build.py
@@ -36,6 +36,10 @@ if __name__ == "__main__":
     builder.make_dirs(args)
     builder.setup_logger(args)
 
+    builder.install_packages(args, [
+        "libgmp3-dev=2:6.2.*",
+    ])
+
     builder.fetch_source(
         args=args,
         source=f"https://ftp.gnu.org/gnu/gdb/gdb-{args.version}.tar.gz",
@@ -43,66 +47,24 @@ if __name__ == "__main__":
     )
 
     source_dir = os.path.join(args.source_dir, f"gdb-{args.version}")
+    env = builder.create_standard_envvars(args)
 
-    if args.target == builder.TARGET_LINUX_arm64_v8a and "x86" in os.uname().machine:
-        builder.install_packages(args, [
-            "g++-7",
-            "gcc-7",
-            "libgmp3-dev=2:6.2.*",
-            "texinfo=6.7.0*",
-        ])
-        
-        builder.execute(args, [
-            "update-alternatives",
-            "--install",
-            "/usr/bin/gcc",
-            "gcc",
-            "/usr/bin/gcc-7",
-            "1",
-        ])
-        builder.execute(args, [
-            "update-alternatives",
-            "--install",
-            "/usr/bin/g++",
-            "g++",
-            "/usr/bin/g++-7",
-            "1",
-        ])
-
-        logging.root.info("Configuring cross-compilation build")
-        builder.execute(args, [
-            "./configure",
-            "--target=aarch64-linux-gnu",
-            "--host=x86_64-unknown-linux-gnu",
-            "--prefix=/",
-        ], cwd=source_dir, env={"CC": "", "CXX": "", "AR": "ar"})
-
-    else:
-        builder.install_packages(args, [
-            "libgmp3-dev=2:6.2.*",
-        ])
-
-        env = builder.create_standard_envvars(args)
-        env["AR"] = "ar"
-
-        logging.root.info("Configuring build")
-        builder.execute(args, [
-            "./configure",
-            "--host=x86_64-linux-gnu",
-            "--prefix=/",
-        ], cwd=source_dir, env=env)
+    logging.root.info("Configuring build")
+    builder.execute(args, [
+        "./configure",
+        "--prefix=/",
+    ], cwd=source_dir, env=env)
 
     logging.root.info("Building")
     builder.execute(args, [
         "make",
-        "V=1",
         "-j",
         args.num_threads,
-    ], cwd=source_dir)
+    ], cwd=source_dir, env=env)
     builder.execute(args, [
         "make",
         f"DESTDIR={args.install_dir}",
         "install",
-    ], cwd=source_dir)
+    ], cwd=source_dir, env=env)
 
     builder.create_package(args)
